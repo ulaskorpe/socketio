7ddaba43a58dafbe3603fe0134dbccf543a9cef1
ulaskorpe <ulaskorpe@gmail.com>
2018-02-23 12:20:17 +0000

    broadcast

--- js/server.js
+++ js/server.js
@@ -12,15 +12,20 @@ console.log('socketio server created');
 io.on('connection',function(socket){
 
 
-    socket.emit('alert','hello from socketio server'+ new Date());
-/*
-    socket.on('click',function(data){
-        console.log(data);
+    socket.emit('alert','hello from socketio server'+ new Date() + ":" + Math.random());
+
+    socket.on('color',function(data){
+    //   console.log('color server.js'+data);
+        socket.broadcast.emit('color',data);
     });
 
     // socket.emit('click','hello from the gutter '+new Date());
 
+
+
     socket.on('disconnect',function(){
         console.log('smo disconnected!');
-    })*/
+    })
+
+
 });
